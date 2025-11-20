c4fc29d86b9753f083d973735f6794edd0805706
Yasminisasleep <Benaliyasmine904@gmail.com>
2025-11-20 14:32:56 +0100

    first commit

---
+++ src/main/java/com/epita/projet/projet_yasmine_benali/ProjetYasmineBenaliApplication.java
@@ -0,0 +1,13 @@
+package com.epita.projet.projet_yasmine_benali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetYasmineBenaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetYasmineBenaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epita/projet/projet_yasmine_benali/ProjetYasmineBenaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epita.projet.projet_yasmine_benali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetYasmineBenaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
